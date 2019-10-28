3d3d0f7be5d14fb4fed06cfcb2e287f20dc584f4
A K <akunz@alum.mit.edu>
2019-10-28 11:58:51 -0400

    set up data handling branch (#1)

--- pages/blog/[post].js
+++ pages/blog/[post].js
@@ -1,22 +1,13 @@
 import React from 'react'
-import fetch from 'isomorphic-unfetch'
+import { useRouter } from 'next/router'
 
-function Post({ name, stars }) {
+function Post() {
+  const router = useRouter()
   return (
     <>
-      <h3>
-        PSA: {name} now has {stars} on GitHub
-      </h3>
+      <h3>Router query: {JSON.stringify(router.query)}</h3>
     </>
   )
-}
-
-Post.getInitialProps = async ctx => {
-  const { query } = ctx
-  console.log('query found: ', ctx)
-  const res = await fetch('https://api.github.com/repos/zeit/next.js')
-  const json = await res.json()
-  return { stars: json.stargazers_count, name: json.name }
 }
 
 export default Post
