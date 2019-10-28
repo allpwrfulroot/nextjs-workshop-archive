61d55ec40ffada6ea1921d73f15674283c307f11
allpwrfulroot <akunz@alum.mit.edu>
2019-10-28 07:46:50 -0400

    set up data handling branch

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -1,11 +1,10 @@
 import React from 'react'
 import Link from 'next/link'
-import fetch from 'isomorphic-unfetch'
 
-function Blog({ stars }) {
+function Blog() {
   return (
     <>
-      <h3>PSA: NextJS now has {stars} on GitHub</h3>
+      <h3>Blog posts placeholder</h3>
       <ul>
         <li>
           <Link href="/blog/[post]" as="/blog/a-meditation-23948298">
@@ -15,13 +14,6 @@ function Blog({ stars }) {
       </ul>
     </>
   )
-}
-
-Blog.getInitialProps = async () => {
-  const res = await fetch('https://api.github.com/repos/zeit/next.js')
-  const json = await res.json()
-  console.log('json? ', json)
-  return { stars: json.stargazers_count }
 }
 
 export default Blog
